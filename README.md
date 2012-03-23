7bbd7768a6c998c3bb7ae6df9f363a4a0544cde6
db42 <dushyant37@gmail.com>
2012-03-23 19:11:33 +0530

    Use locks to make it thread-safe for parallel accesses

--- README.md
+++ README.md
@@ -43,3 +43,28 @@ python cache.py
 cd path/to/PyCache
 nosetests
 ```
+### Notes
+
+#### 1. GIL in CPython
+Though dictionary accesses are thread-safe in CPython due to Global Interpreter Lock, I have used a lock to protect dict to make this code implementation independent. 
+http://effbot.org/pyfaq/what-kinds-of-global-value-mutation-are-thread-safe.htm
+
+
+```
+""" Write (key,value) into the cache """
+with self.hash_db_lock:
+	self.hash_db[key] = Elem(value)
+```
+
+#### 2. Assumption of correct usage of endRead() function
+Here, I have assumed that the application using this object cache will make a single call to endRead() corresponding to every beginRead() call.
+
+Usage:
+```
+self.cache.write("test_key", "test_value")
+
+print self.cache.beginRead("test_key")
+
+self.cache.endRead("test_key")
+self.cache.endRead("test_key") #Not allowed
+```
--- cache.py
+++ cache.py
@@ -3,53 +3,68 @@ Created on 15-Mar-2012
 
 @author: dushyant
 '''
+import threading
 
 class Elem(object):
     """ This class corresponds to element of Cache. Can be further modified to extend this cache """
     
     def __init__(self, value):
         self.value = value
-        self.evict_status = True #Ready to evict
+        self.readers_count = 0 #Ready to evict
+        self.count_lock = threading.Lock()
 
     def getValue(self):
         return self.value
     
-    def setEvictStatus(self, status):
-        self.evict_status = status
+    def beginRead(self):
+        with self.count_lock:
+            self.readers_count += 1
     
+    def endRead(self):
+        with self.count_lock:
+            self.readers_count -= 1
+            assert self.readers_count >= 0
+                    
     def getEvictStatus(self):
-        return self.evict_status
+        if self.readers_count == 0:
+            return True
+        else:
+            return False
     
 class Cache(object):
     """ Base class for Cache object """
     def __init__(self, size):
         self.hash_db = {}
         self.size = size
+        self.hash_db_lock = threading.Lock()
         
     def beginRead(self, key):
         """ Return the value corresponding to key from the cache. If key is not present, throws exception"""
-        if self.hash_db.has_key(key):
-            elem = self.hash_db[key]
-        else:
-            raise LookupError
-        elem.setEvictStatus(False)
+        with self.hash_db_lock:
+            if self.hash_db.has_key(key):
+                elem = self.hash_db[key]
+            else:
+                raise LookupError
+        elem.beginRead()
         return elem.getValue()
     
     def endRead(self, key):
         """ Return None. If key is not present, throws exception. Now, entry corresponding to key can be evicted from the cache """
-        if self.hash_db.has_key(key):
-            self.hash_db[key].setEvictStatus(True)
-        else:
-            raise LookupError
+        with self.hash_db_lock:
+            if self.hash_db.has_key(key):
+                self.hash_db[key].endRead()
+            else:
+                raise LookupError
         return
     
     def write(self, key, value):
         """ Write (key,value) into the cache """
-        self.hash_db[key] = Elem(value)
+        with self.hash_db_lock:
+            self.hash_db[key] = Elem(value)
         
-        #if size exceeds cache_size
-        if (len(self.hash_db) > self.size):
-            self.__evictEntry()
+            #if size exceeds cache_size
+            if (len(self.hash_db) > self.size):
+                self.__evictEntry()
     
     def __evictEntry(self):
         keys = [key for key in self.hash_db.keys() if self.hash_db[key].getEvictStatus()]
